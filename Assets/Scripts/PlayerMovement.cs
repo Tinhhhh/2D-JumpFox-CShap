1e96269cc0c387926e4073b26d2720e6a471fd64
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2024-05-19 20:11:14 +0700

    feat: add bounciness

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,15 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Config")]
     [SerializeField] private float speed = 8f;
-    [SerializeField] private float jumpDelay = 0.75f;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private CapsuleCollider2D bc;
     [SerializeField] private float jumpValue = 0.0f;
+    [SerializeField] private CapsuleCollider2D cc;
     [SerializeField] private PhysicsMaterial2D bounceMat, normalMat;
 
 
@@ -17,49 +17,53 @@ public class PlayerMovement : MonoBehaviour
     private float dirY;
     private Rigidbody2D rb;
     public bool canJump = true;
-    private float lastJumpTime;
+    private BoxCollider2D bc;
 
     private PlayerAnimation playerAnimation;
 
     // Start is called before the first frame update
     void Awake()
     {
-        bc = GetComponent<CapsuleCollider2D>();
+        cc = GetComponent<CapsuleCollider2D>();
+        bc = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
         playerAnimation = GetComponent<PlayerAnimation>();
     }
 
     void Start()
     {
-        lastJumpTime = -jumpDelay;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Movement();
+        if (IsGrounded())
+        {
+            canJump = true;
+            Movement();
+        }
+        Jump();
 
         //chỉ cho di chuyển khi không nhảy và đang chạm đất
         if (jumpValue == 0 && IsGrounded())
         {
+            bc.sharedMaterial = normalMat;
             rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
         }
 
-        Jump();
         if (IsGrounded())
         {
             dirY = 0;
             playerAnimation.SetJumpingAnimation(dirY);
         }
 
-        if (jumpValue > 0.0f && !Input.GetKey("space"))
+        if (jumpValue > 0.0f)
         {
-            rb.sharedMaterial = bounceMat;
+            playerAnimation.setHoldJumpAnimation(true);
         }
         else
         {
-            rb.sharedMaterial = normalMat;
+            playerAnimation.setHoldJumpAnimation(false);
         }
 
 
@@ -71,6 +75,8 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey("space") && IsGrounded() && canJump)
         {
             jumpValue += 0.5f;
+            changeMaterial();
+
         }
 
         //một khi đã ấn nút nhảy thì sẽ khiến nhân vật đứng lại ngay lập tức
@@ -90,13 +96,12 @@ public class PlayerMovement : MonoBehaviour
         //thả nút nhảy thì thực hiện nhảy
         if (Input.GetKeyUp("space"))
         {
-            lastJumpTime = Time.time;
             if (IsGrounded())
             {
                 rb.velocity = new Vector2(dirX * speed, jumpValue);
                 jumpValue = 0.0f;
             }
-            canJump = true;
+            // canJump = true;
             //Set the animator MoveY
             dirY = rb.velocity.y;
             playerAnimation.SetJumpingAnimation(rb.velocity.y);
@@ -110,7 +115,6 @@ public class PlayerMovement : MonoBehaviour
         canJump = false;
         jumpValue = 0.0f;
     }
-
 
 
     public void Movement()
@@ -127,8 +131,9 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             playerAnimation.SetIdleAnimation(true);
+            playerAnimation.SetMovingAnimation(dirX);
+
         }
-        playerAnimation.SetMovingAnimation(dirX);
 
     }
 
@@ -136,17 +141,23 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         //kiểm tra 
-        return Physics2D.OverlapCircle(bc.bounds.center, bc.bounds.size.x, groundLayer);
+        // return Physics2D.OverlapCircle(cc.bounds.center, cc.bounds.size.y, groundLayer);
+        return Physics2D.OverlapBox(cc.bounds.center, cc.bounds.size, 0f, groundLayer);
         // return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void changeMaterial()
+    {
+        bc.sharedMaterial = bounceMat;
     }
 
     private void OnDrawGizmos()
     {
         // Lấy kích thước của Collider2D
-        Vector2 size = bc.bounds.size;
+        Vector2 size = cc.bounds.size;
 
         // Lấy vị trí của Collider2D
-        Vector3 position = bc.bounds.center;
+        Vector3 position = cc.bounds.center;
 
         Gizmos.color = Color.yellow;
         // Gizmos.DrawWireSphere(groundCheck.position, 0.2f);
